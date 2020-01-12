5dce79f2713050ecee112776c2ed1e02f128544d
Marijo Kiš <marijo.kis@gauss.hr>
2020-01-12 01:24:50 +0100

    feat: login page layout

---
+++ layouts/empty.vue
@@ -0,0 +1,6 @@
+<template>
+  <v-app style="background-color: #E0F2F1">
+    <nuxt/>
+  </v-app>
+</template>
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,124 +1,36 @@
 <template>
-  <v-container fluid>
-    <v-card style="border-radius: 16px" elevation="5">
-      <v-card-title><span class="teal--text font-weight-bold headline">Student Information Form</span></v-card-title>
-      <v-divider></v-divider>
-      <v-card-text>
-        <v-layout row wrap class="py-4 px-5">
-          <v-flex xs4 mt-2 px-4>
-            <v-select outlined label="School" color="teal" :items="schoolItems" :item-text="schoolItems.text"
-                      :item-value="schoolItems.value"></v-select>
-          </v-flex>
-          <v-flex xs4 mt-2 px-4>
-            <v-select outlined label="Sex" color="teal" :items="sexItems" :item-text="sexItems.text"
-                      :item-value="sexItems.value"></v-select>
-          </v-flex>
-          <v-flex xs4 mt-2 px-4>
-            <v-select outlined label="Age" color="teal" :items="ageItems"></v-select>
-          </v-flex>
-          <v-flex xs4 mt-2 px-4>
-            <v-select outlined label="Address" color="teal" :items="addressItems" :item-text="addressItems.text"
-                      :item-value="addressItems.value"></v-select>
-          </v-flex>
-          <v-flex xs4 mt-2 px-4>
-            <v-select outlined label="Family size" color="teal" :items="familyItems" :item-text="familyItems.text"
-                      :item-value="familyItems.value"></v-select>
-          </v-flex>
-          <v-flex xs4 mt-2 px-4>
-            <v-select outlined label="Parent's status" color="teal" :items="pstatusItems" :item-text="pstatusItems.text"
-                      :item-value="pstatusItems.value"></v-select>
-          </v-flex>
-          <v-flex xs4 mt-2 px-4>
-            <v-select outlined label="Mother's education" color="teal" :items="meduItems" :item-text="meduItems.text"
-                      :item-value="meduItems.value"></v-select>
-          </v-flex>
-          <v-flex xs4 mt-2 px-4>
-            <v-select outlined label="Fathers's education" color="teal" :items="feduItems" :item-text="feduItems.text"
-                      :item-value="feduItems.value"></v-select>
-          </v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-          <v-flex xs4 mt-2 px-4></v-flex>
-        </v-layout>
-      </v-card-text>
-      <v-divider></v-divider>
-      <v-card-actions>
-        <v-btn class="ma-3 white--text" rounded outlined color="teal" large>Reset</v-btn>
-        <v-btn class="ma-3 white--text" rounded depressed color="teal" large>Predict</v-btn>
-      </v-card-actions>
-    </v-card>
+  <v-container grid-list-md fluid fill-height>
+    <v-layout justify-center align-center>
+      <v-flex xs12 sm8 md6 lg4 xl3>
+        <v-card max-width="500" style="border-radius: 16px">
+          <v-card-title style="background-color: #009688" class="white--text headline font-weight-bold">Login
+          </v-card-title>
+          <v-divider></v-divider>
+          <v-card-text>
+            <v-text-field v-model="user.username" label="Username" color="teal"/>
+            <v-text-field v-model="user.password" label="Password" color="teal"/>
+          </v-card-text>
+          <v-divider></v-divider>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn class="mr-2 mt-1 white--text" rounded depressed color="teal">login</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
 
 <script>
     export default {
+        layout: 'empty',
         data() {
             return {
-                schoolItems: [{value: "GP", text: "Gabriel Pereira"}, {value: "MS", text: "Mousinho da Silveira"}],
-                sexItems: [{value: "F", text: "Female"}, {value: "M", text: "Male"}],
-                ageItems: [15, 16, 17, 18, 19, 20, 21, 22],
-                addressItems: [{value: "U", text: "Urban"}, {value: "R", text: "Rural"}],
-                familyItems: [{value: "LE3", text: "Less or equal to 3"}, {value: "GT3", text: "Greater than 3"}],
-                pstatusItems: [{value: "T", text: "Living together"}, {value: "A", text: "Living apart"}],
-                meduItems: [
-                    {value: 0, text: "None"},
-                    {value: 1, text: "Primary education (4th grade)"},
-                    {value: 2, text: "5th to 9th grade"},
-                    {value: 3, text: "Secondary education"},
-                    {value: 4, text: "Higher education"},
-                ],
-                feduItems: [
-                    {value: 0, text: "None"},
-                    {value: 1, text: "Primary education (4th grade)"},
-                    {value: 2, text: "5th to 9th grade"},
-                    {value: 3, text: "Secondary education"},
-                    {value: 4, text: "Higher education"},
-                ],
+                user: {
+                    username: "",
+                    password: ""
+                }
             }
         }
     }
